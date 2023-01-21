9e9f4ea3fcfe3616d4f118b82ef17d4a0c78dd9b
andyBzg <andr.bzg@gmail.com>
2023-01-21 01:58:22 +0100

    cruiser add

--- src/FieldGenerator.java
+++ src/FieldGenerator.java
@@ -5,6 +5,22 @@ public class FieldGenerator {
     private int length;
     private int width;
 
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
     public FieldGenerator(int length, int width) {
         this.length = length;
         this.width = width;
@@ -14,7 +30,47 @@ public class FieldGenerator {
         Random random = new Random();
         int[][] field = new int[length][width];
 
-        for (int i = 0; i < 4; i++) {
+        //simpleShip - одна клетка х 4
+        //destroyer - 2 клетки х 3
+        //cruiser - 3 клетки х 2
+        //battleship - 4 клетки х 1
+
+
+
+        int destroyer = 0;
+        while (destroyer < 3) {
+            int a = random.nextInt(0, length);
+            int b = random.nextInt(0, width - 1);
+
+            if (field[a][b] == 0) {
+                field[a][b] = 2;
+                destroyer++;
+            }
+        }
+
+        for (int i = 0; i < field.length; i++) {        //идём по строчкам
+            for (int j = 0; j < field[i].length; j++) { //по каждому элементу в строчке
+
+                if (field[i][j] == 2) {
+                    j++;
+                    field[i][j] = 2;
+                }
+            }
+        }
+
+        int simpleShip = 0;
+        while (simpleShip < 4) {
+            int a = random.nextInt(0, length);
+            int b = random.nextInt(0, width);
+
+            if (field[a][b] == 0) {
+                field[a][b] = 1;
+                simpleShip++;
+            }
+
+        }
+
+        /*for (int i = 0; i < 50; i++) {
             int a = random.nextInt(0, length);
             int b = random.nextInt(0, width);
 
@@ -22,7 +78,7 @@ public class FieldGenerator {
                 i--;
             }
             field[a][b] = 1;
-        }
+        }*/
 
         return field;
     }
