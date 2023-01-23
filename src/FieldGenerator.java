a622fbd1934c7a292b5ed3a8975cafc6aeeeeb9f
andyBzg <andr.bzg@gmail.com>
2023-01-23 18:52:36 +0100

    Add battleship generation

--- src/FieldGenerator.java
+++ src/FieldGenerator.java
@@ -38,8 +38,38 @@ public class FieldGenerator {
         //cruiser - 3 клетки х 2
         //battleship - 4 клетки х 1
 
+        int battleship = 0;
+        while (battleship < 1) {
+            int a = random.nextInt(0, height - 3);
+            int b = random.nextInt(0, width - 3);
+
+            if (field[a][b] == 0) {
+                field[a][b] = 4;
+                battleship++;
+            }
+        }
+
+        boolean battleshipRotation = random.nextBoolean();
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[i][j] == 4 && battleshipRotation) {
+                    for (int k = 0; k < 3; k++) {
+                        j++; // rotation == true - горизонтальное отображение
+                        field[i][j] = 4;
+                    }
+                }
+                else if (field[i][j] == 4 && !battleshipRotation) {
+                    for (int k = 0; k < 3; k++) {
+                        i++; // rotation == false - вертикальное отображение
+                        field[i][j] = 4;
+                    }
+                }
+            }
+        }
+
         int destroyer = 0;
-        while (destroyer < 3) {
+        while (destroyer < 4) {
             int a = random.nextInt(0, height);
             int b = random.nextInt(0, width - 1);
 
@@ -60,7 +90,7 @@ public class FieldGenerator {
         }
 
         int simpleShip = 0;
-        while (simpleShip < 4) {
+        while (simpleShip < 6) {
             int a = random.nextInt(0, height);
             int b = random.nextInt(0, width);
 
